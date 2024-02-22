174c93a39ec8ab83876680e09dd42929244d659f
St0pien <jstepien50@gmail.com>
2024-02-22 19:21:22 +0100

    Window resize remaps

--- nvim/lua/st0pien/remap.lua
+++ nvim/lua/st0pien/remap.lua
@@ -23,3 +23,8 @@ end)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "=", "<cmd>vertical resize +5<cr>")
+vim.keymap.set("n", "-", "<cmd>vertical resize -5<cr>")
+vim.keymap.set("n", "+", "<cmd>horizontal resize +5<cr>")
+vim.keymap.set("n", "_", "<cmd>horizontal resize -5<cr>")
