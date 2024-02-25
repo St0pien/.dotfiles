e9f58df55f5a624448b0c3aa149f9848ecb3a787
St0pien <jstepien50@gmail.com>
2024-02-25 19:42:18 +0100

    Switch eslint to eslint_d

--- nvim/after/plugin/null_ls.lua
+++ nvim/after/plugin/null_ls.lua
@@ -3,6 +3,7 @@ local null_ls = require('null-ls')
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.autopep8
 	}
 })
