c989cd0160e5449d0981c58c3ac759577927b31d
St0pien <jstepien50@gmail.com>
2024-05-12 23:07:51 +0200

    Fix performance issue

--- nvim/after/plugin/linter.lua
+++ nvim/after/plugin/linter.lua
@@ -5,19 +5,6 @@ require("lint").linters_by_ft = {
 	typescriptreact = { "eslint_d" },
 }
 
-local eslint = require("lint").linters.eslint_d
-
-eslint.args = {
-	"--no-warn-ignored", -- <-- this is the key argument
-	"--format",
-	"json",
-	"--stdin",
-	"--stdin-filename",
-	function()
-		return vim.api.nvim_buf_get_name(0)
-	end,
-}
-
 vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave", "TextChanged" }, {
 	callback = function()
 		require("lint").try_lint()
